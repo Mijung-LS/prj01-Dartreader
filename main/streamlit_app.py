045935d9810acfb049d9564441e99c8ec3afa7d9
Mijung-LS <159690645+Mijung-LS@users.noreply.github.com>
2024-02-11 19:07:07 +0900

    Create streamlit_app.py

---
+++ main/streamlit_app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+# test
+import streamlit as st
+
+start_color, end_color = st.select_slider(
+    'Select a range of color wavelength',
+    options=['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'],
+    value=('red', 'blue'))
+st.write('You selected wavelengths between', start_color, 'and', end_color)
